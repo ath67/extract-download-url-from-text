2d8d9b73aa8d5fe92b5218b7a0d0f182f67dff75
athossampayo <56121956+athossampayo@users.noreply.github.com>
2019-10-04 15:07:41 -0300

    Update (Screen print cleaned; delete filesOutput)
    Cleaned screen messages.
    Added fase for user approve ou not the URLS links.
    Tried to delete links not downloaded when user does not approve the list but not implemented yet.

--- downloader.py
+++ downloader.py
@@ -13,24 +13,28 @@ arquivo = input("Type the filename with the extension. The file should be on the
 a = open(arquivo, 'r') #reading file with original text
 text = a.read()
 a.close()
+print ('-'*80) 
+print("Text selected:")
 print(text)                 #print original text
+print ('-'*80) 
 a1=text.replace("\n", " ")  #puts everything in only one line (deletes any '\n')
 a1=a1.replace("http", " http")  #creates a space between any frase that have "XXX:http" so split() consider the http a separete word
 a11=a1.split()              #creates a list that every text in the a1 is an item
-print(a1)                   #prints the text before splitting
-print("splits=", str(a11))  #prints the split() list
 
-print(type(a11))            #print a check to make sure a11 is a list
+if type(a11)!= list:    #print a check to make sure a11 is a list
+   print ("Error with text list generated, please contact dev")
+   time.sleep(60)
+   exit()
 
 
 y=1
 linksVAR=set()
+linksREAD=set()
+print ('-'*80)
+print ('-'*80) 
 for string in a11:          #loop that will filter every list item that has "http"
     if 'http' in string:
-        linksVAR.add(string)
-        print(linksVAR)
-        print(string)        #prints the item with "http"
-        print("Link number:", y)  #Prints an couter of processed urls
+        linksVAR.add(string) 
         opn=open('url-list.txt', 'a')  #creates file for output if already doesnt exists
         opn.close()
         textfinal = open('url-list.txt', 'r+')  #appends links to an .txt file on script's root
@@ -39,9 +43,26 @@ for string in a11:          #loop that will filter every list item that has "htt
            linksVAR.remove(string)
            
         else:
+           print("Link # in text:", y, ". URL:", string)  #Prints an couter of processed urls and it's URL/item with http
+           linksREAD.add(string)
+           time.sleep(0.3)
            textfinal.write(string + '\n')
            textfinal.close()
         y=y+1                #sum to the couter
+print ('-'*80)
+print ('-'*80) 
+print("Waiting 15 sec for checking the URLs above. If there's none, you already run every link!")
+time.sleep(15)
+check=input("If everything is fine, type 'OK' (all caps), if not type anything different:  ")   #checks if user approves list of links
+if check != "OK":
+   print("Deleting links above from output file and closing scprit in 10 sec...")
+   outputx= open('url-list.txt', 'r+')    #Try to delete the list saved on the set named linksREAD //Still to correct, not working
+   for line in outputx:
+      for word in linksREAD:
+         line=line.replace(word, "")
+      outputx.write(line)
+   time.sleep(10)
+   exit()
 
 
 
@@ -52,14 +73,16 @@ if not os.path.isdir(mypath):       #creates the directory/paste if it doesnt ex
     os.makedirs(mypath)
 
 
-print(linksVAR)
 s= len(linksVAR) - (len(linksVAR)-1)
+print('There are ', len(linksVAR), ' items to download.') #prints list length (number of itens to download)
+print ('-'*80)
+print ('-'*80)
 for i in linksVAR:       #loop that downloads every url in a 'clean file'
     linkbaixar= i     #reads list itens
-    print('There are ', len(linksVAR), ' items to download.') #prints list length (number of itens to download)
     print('__________downloading item nº', s, ':', i) #prints current item wich is being downloaded
     wget.download(linkbaixar, out=mypath)           #wget's the item/url
     s += 1
-
-print("Finished downloading! This message will stay on scree for 1 minute")
+print ('-'*80)
+print ('-'*80)
+print("Finished downloading! This message will stay on scree for 40 sec! Bye!!")
 time.sleep(60)
